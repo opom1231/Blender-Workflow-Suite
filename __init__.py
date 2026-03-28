bf398c825595a15f98f5ff0bb40a184424abba4a
Yoseph <yoseph908@gmail.com>
2026-03-28 16:55:45 -0400

    Added bl_info to init

---
+++ __init__.py
@@ -0,0 +1,10 @@
+bl_info = {
+    "name": "Custom Pivot Point Operators",
+    "author": "Yoseph Amare",
+    "version": (1, 0),
+    "blender": (4, 5, 0),
+    "location": "3D View > Pivot Shortcuts",
+    "description": "Adds visible custom operators to set pivot point modes",
+    "category": "3D View",
+}
+
