4f15b3149dc7d74ba25c5661949b51c9563a0195
Yoseph <yoseph908@gmail.com>
2026-03-28 17:16:23 -0400

    Added set pivot operator class

--- __init__.py
+++ __init__.py
@@ -8,3 +8,27 @@ bl_info = {
     "category": "3D View",
 }
 
+# Custom Operators
+
+import bpy
+class set_pivot_median(bpy.types.Operator):
+    """Set Pivot to Median Point"""
+    bl_idname = "view3d.pivot_median"
+    bl_label = "Set Pivot to Median Point"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):
+        context.scene.tool_settings.transform_pivot_point = 'MEDIAN_POINT'
+        return {'FINISHED'}
+
+# Registering
+
+def register():
+    bpy.utils.register_class(set_pivot_median)
+
+def unregister():
+    bpy.utils.unregister_class(set_pivot_median)
+
+if __name__ == "__main__":
+    register()
+
