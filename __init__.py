6501cea9c0012fc3eabb491c06f989275673f2b6
Yoseph <yoseph908@gmail.com>
2026-03-28 18:15:45 -0400

    Added key map logic for set pivot median

--- __init__.py
+++ __init__.py
@@ -21,13 +21,30 @@ class set_pivot_median(bpy.types.Operator):
         context.scene.tool_settings.transform_pivot_point = 'MEDIAN_POINT'
         return {'FINISHED'}
 
+# Keymap storage
+
+addon_keymaps = []
+
 # Registering
 
 def register():
     bpy.utils.register_class(set_pivot_median)
 
+    # Accessing Window Manager to setup keybinds through Keyboard Configuration
+    wm = bpy.context.window_manager
+    kc = wm.keyconfigs.addon
+
+    if kc:
+        # New keymap group for the 3D View
+        km = kc.keymaps.new(name='3D View', space_type='VIEW_3D')
+        # Key map input
+        kmi = km.keymap_items.new(set_pivot_median.bl_idname, 'UP_ARROW', 'PRESS', ctrl=True, shift=True)
+
+        addon_keymaps.append((km, kmi))
+
 def unregister():
     bpy.utils.unregister_class(set_pivot_median)
+    addon_keymaps.clear()
 
 if __name__ == "__main__":
     register()
